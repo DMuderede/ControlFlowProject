6ef544aa54d6f985d131d5befd7596242be45440
Doreen Muderede <doreen.muderede@eduvos.com>
2022-08-10 20:46:43 +0200

    Initial commit

---
+++ src/Controls/Question4.java
@@ -0,0 +1,12 @@
+package Controls;
+
+public class Question4 {
+    public static void main(String args[]){
+
+        int i=1;
+        while(i*i<500)
+        {
+            System.out.println(i*i);
+            i++;
+        }
+}}
---
+++ src/Controls/Question5.java
@@ -0,0 +1,16 @@
+package Controls;
+
+public class Question5 {
+
+    public static void main(String args[]){
+
+
+        for(int rows = 0; rows <= 3; rows++){
+            //inner loop to print the numbers
+            for(int columns = 0; columns <= 6; columns++){
+                System.out.print(" " + "5");
+
+            }
+            System.out.println(" ");
+        }
+}}
